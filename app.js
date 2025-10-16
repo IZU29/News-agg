1055137345f8026666eed0b48f8afcad17dcff71
Izuchukwu <izunduche29@gmail.com>
2025-10-16 19:59:08 +0100

    The Login and Register Controllers are functioning properly

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+require('dotenv').config()
+
 const Express = require('express')
 const app = Express()
 const UserRouter = require("./routes/user")
 const ConnectDB = require("./db/connect")
 
-
+app.use(Express.json())
 app.use('/api' , UserRouter)
 
 
 app.listen(3000 , ()=>{
-    ConnectDB()
+    ConnectDB(process.env.MONGO_URI)
     console.log(`Server Is Listening on Port ${3000}`)
 })
--- models/user.js
+++ models/user.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
     username : {
         type : String,
+        unique : true,
         required : [true , "Please Provide Username"]
     },
     email : {
         type : String,
+        unique : true,
         required : [true , "Please Provide Email"]
     },
     password : {
